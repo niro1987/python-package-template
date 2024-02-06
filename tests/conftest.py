bd897e80b6323ef31f44122e464a1d68c309ca0a
Niels Perfors <niro1987@users.noreply.github.com>
2024-02-06 13:06:40 +0000

    add pytest-socket
    Prevens unintended network requests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,27 @@
 """
 This is a configuration file for pytest containing customizations and fixtures.
 """
+import pytest
+from pytest_socket import disable_socket
+
+
+def pytest_runtest_setup():
+    """Disable socket calls during tests to ensure network calls are prevented."""
+    disable_socket()
+
+
+@pytest.fixture(name="message")
+def hello_world() -> str:
+    """
+    Example fixture
+
+    Returns
+        str : 'Hello World'
+
+    Use this fixture in a testcase like:
+
+    def test_hello_world(message: str) -> None:
+        print(message) # will print 'Hello World'
+        ...
+    """
+    return "Hello World"
